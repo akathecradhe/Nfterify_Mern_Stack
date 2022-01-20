7f7606132211385dce8517dffb649ba56e6242c6
c1811006 <tsunguc@cardiff.ac.uk>
2022-01-20 19:54:31 +0000

    feat: create basic routes to GetALL & Create
    Signed-off-by: c1811006 <tsunguc@cardiff.ac.uk>

---
+++ server/controller/AdminController.js
@@ -0,0 +1,30 @@
+import itemModel from "../models/itemModel";
+
+// fetches All the Items from the database.
+const findAll = (req, res) => {
+
+    itemModel.find({},(err, result) => {
+        if(err) {
+            console.log(err)
+        }
+        else{
+            console.log(typeof(result));
+            res.sendStatus(200).json(result);
+        }
+    })
+};
+
+// Create new item to mint
+const create =async (req, res) => {
+    // fata form client is requested into the body
+    const item= req.body;
+    const newItem = new itemModel(item);
+    //
+    await newItem.save();
+
+    //sending the new data to the front end to confirm that the right data is passed to backend
+    res.sendStatus(201).json(item);
+
+};
+
+export {create, findAll}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {create, findAll} from '../controller/AdminController.js';
 import cors from 'cors';
 
 
@@ -8,6 +9,15 @@ const app= express();
 app.use(express.json());
 //all cross object referencing from front end
 app.use(cors());
+
+
+///ADMIN
+//get all items
+app.get('/getItems',findAll);
+
+//create item for minting
+app.post('/createItems',create);
+
 
 app.get('/', (req, res) => {
     res.json({message:'Hello World!'})
