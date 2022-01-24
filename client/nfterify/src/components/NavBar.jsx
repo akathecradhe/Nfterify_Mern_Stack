5e5b409b21b4a773921d140cdbe6ec3ee2e2eb87
clive tsungu <tsunguc@cardiff.ac.uk>
2022-01-24 09:03:55 +0000

    Feat: css added to navbar

--- client/nfterify/src/components/NavBar.jsx
+++ client/nfterify/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar (){
 
     return (
         <div>
-            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+            <nav className="navbar navbar-light bg-info ">
                 <div className="container flex flex-wrap justify-between items-center mx-auto">
                     <a href="/" className="flex">
                         <span
