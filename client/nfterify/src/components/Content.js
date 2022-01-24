5543611871f9d1ee381483e888950b14ce9979b9
c1811006 <tsunguc@cardiff.ac.uk>
2022-01-24 06:11:12 +0000

    Merge: merged login and registration to main UI branch
    Signed-off-by: c1811006 <tsunguc@cardiff.ac.uk>

--- client/nfterify/src/components/Content.js
+++ client/nfterify/src/components/Content.js
@@ -3,6 +3,7 @@ import MainSection from "./MainSection";
 function Content() {
 
     return(
+
         <section className="relative py-16 bg-gray-100">
             <div className="container max-w-7xl px-4 mx-auto">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-2xl -mt-64">
