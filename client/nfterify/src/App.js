57b01117ff3c8ced6e42ad05f623fe74977bddff
Clive Tsungu <ctsungu@gmail.com>
2022-03-07 09:34:07 +0000

    setting up build folder

--- client/nfterify/src/App.js
+++ client/nfterify/src/App.js
@@ -1,68 +1,61 @@
-import CreateItem from './components/CreateItem'
+import CreateItem from './components/CreateItem';
 import './App.css';
-import Content from './components/Content'
-import UserContent from './components/userComponets/UserContent'
-import MintItem from './components/userComponets/MintItem'
-import ItemDetail from './components/ItemDetail'
-import ListItems from './components/ListItems'
-import AdminHome from './Pages/AdminHome'
-import Register from './components/Register'
-import Login from './components/Login'
-import PrivateRoute from './components/PrivateRoute'
-import NavBar from "./components/NavBar";
-import {
-    Switch,
-    Route,
-} from "react-router-dom";
-
+import Content from './components/Content';
+import UserContent from './components/userComponets/UserContent';
+import MintItem from './components/userComponets/MintItem';
+import ItemDetail from './components/ItemDetail';
+import ListItems from './components/ListItems';
+import AdminHome from './Pages/AdminHome';
+import Register from './components/Register';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import NavBar from './components/NavBar';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-        <NavBar />
+      <NavBar />
 
-        <Switch>
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
 
-            <Route path="/register">
-                <Register/>
-            </Route>
-            <Route path="/login">
-                <Login/>
-            </Route>
+        <Route path="/">
+          <Login />
+        </Route>
 
-            //Admin
-            <PrivateRoute exact path='/ListItems'>
-                <ListItems/></PrivateRoute>
+        <PrivateRoute exact path="/ListItems">
+          <ListItems />
+        </PrivateRoute>
 
-            <PrivateRoute exact path="/createItem">
-                <CreateItem/>
-            </PrivateRoute>
+        <PrivateRoute exact path="/createItem">
+          <CreateItem />
+        </PrivateRoute>
 
-            <PrivateRoute exact path="/">
-                <Content/>
-            </PrivateRoute>
+        <PrivateRoute exact path="/dashboard">
+          <Content />
+        </PrivateRoute>
 
-            <PrivateRoute exact path="/item/:itemId">
-                <ItemDetail/>
-            </PrivateRoute>
+        <PrivateRoute exact path="/item/:itemId">
+          <ItemDetail />
+        </PrivateRoute>
 
-            //User
-            <PrivateRoute exact path="/user">
-                <UserContent/>
-            </PrivateRoute>
-            <PrivateRoute exact path="/mint">
-                <MintItem/>
-            </PrivateRoute>
+        <PrivateRoute exact path="/user">
+          <UserContent />
+        </PrivateRoute>
+        <PrivateRoute exact path="/mint">
+          <MintItem />
+        </PrivateRoute>
 
-
-
-            {/* Can also use a named `children` prop */}
-            {/*<Route path="/users/:id" children={<User />} />*/}
-        </Switch>
-
+        {/* Can also use a named `children` prop */}
+        {/*<Route path="/users/:id" children={<User />} />*/}
+      </Switch>
     </>
-
-
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
-import dbConnect from "./database/Dbconnection";
+import dbConnect from './database/Dbconnection';
 import app from './routes/routes';
 import dotenv from 'dotenv';
 
 dbConnect();
-dotenv.config()
+dotenv.config();
 
-const aPORT =  process.env.PORT;
+const aPORT = process.env.PORT || 3001;
 
+// Step 1:
+app.use(express.static(path.resolve(__dirname, '../client/nfterify/build')));
+// Step 2:
+app.get('*', function (request, response) {
+  response.sendFile(
+    path.resolve(__dirname, '../client/nfterify/build', 'index.html')
+  );
+});
 
 app.listen(aPORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${aPORT}`);
+  console.log(`⚡️[server]: Server is running at https://localhost:${aPORT}`);
 });
